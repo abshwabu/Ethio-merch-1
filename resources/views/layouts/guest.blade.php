f036aa22ca28fe6dca11c39b91242f58ae91e967
hshehabu <hamzashehabu457@gmail.com>
2023-01-18 23:14:33 +0300

    support email added

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -40,7 +40,7 @@
 						<div class="topbar-menu left-menu">
 							<ul>
 								<li class="menu-item" >
-									<a title="Hotline: (+123) 456 789" href="#" ><span class="icon label-before fa fa-mobile"></span>Hotline: (+123) 456 789</a>
+									<a title="support@yenesuq.com" href="mailto:support@yenesuq.com" ><span class="icon label-before fa fa-mobile"></span>Hotline: (+123) 456 789</a>
 								</li>
 							</ul>
 						</div>
